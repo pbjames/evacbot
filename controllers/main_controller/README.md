c51720f5f9f3195a2ab1134c03fc79e712995921
James <jameskowal10@gmail.com>
2025-11-12 11:40:33 +0000

    Initial commit

---
+++ controllers/main_controller/README.md
@@ -0,0 +1,21 @@
+# Evacbot Main Controller
+
+This is the code which decides what the robot does. If you have any questions then bring
+them up when we meet or message me on anything!.
+
+## Usage
+
+This should automatically be registered by Webots if this folder is chosen correctly as
+the project directory. This means you don't have to do anything else.
+
+## Plans
+
+To reiterate our proposal:
+
+- Tamryn does SLAM
+- Xiaoyu does hazard detection
+- James does pathing (w/ SLAM + hazards)
+
+## Contribution
+
+Just use well-named commits, and branch if you can. I'll try to lead by example.
---
+++ controllers/main_controller/main_controller.py
@@ -0,0 +1,28 @@
+from controller.robot import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
